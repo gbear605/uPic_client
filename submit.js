f404678e3d16796c7f553e7ab88ee4c7036dd8f4
gbear605 <garrisontaylor@gmail.com>
2014-02-22 20:13:47 -0500

    fix stuff

--- submit.js
+++ submit.js
@@ -1,4 +1,3 @@
-
 	var img1 = document.getElementById("image1form").value;
-	var img2 = document.getElementById("image2form").value;
-	var description = document.getElementById("descriptionform").value;
+	var img2 = document.getElementById("image2form");
+	var description = document.getElementById("descriptionform");
