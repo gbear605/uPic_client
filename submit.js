54e20022ea4663665c9f24b68b91442b1777c070
Christopher Wilson <christopher.wilson413@gmail.com>
2014-02-22 20:11:03 -0500

    ahhhh

--- submit.js
+++ submit.js
@@ -1,4 +1,6 @@
-
+var submit = function(){
 	var img1 = document.getElementById("image1form").value;
 	var img2 = document.getElementById("image2form").value;
 	var description = document.getElementById("descriptionform").value;
+	console.log(img1 + img2 + description);
+}
