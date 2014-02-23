1c09f5a0bd2aa07d0f1593f58212ee0e187eb830
Christopher Wilson <christopher.wilson413@gmail.com>
2014-02-22 20:27:58 -0500

    fix stuff

--- submit.js
+++ submit.js
@@ -1,6 +1,15 @@
-var submit = function(){
+Parse.initialize("wvQUX0mYhNb3pl0AznV8iTslSWPLSjStPQPKvrgd", "kuPoQfcvFIF0H4ez18fzwZ2QQ7Lx6zBJRlNOqBPQ");
+
+
+$('#submitform').on('click', function(event){
+	event.preventDefault();
+	console.log("awj");
 	var img1 = document.getElementById("image1form").value;
 	var img2 = document.getElementById("image2form").value;
 	var description = document.getElementById("descriptionform").value;
-	console.log(img1 + img2 + description);
+	var TestObject = Parse.Object.extend("Polls");
+	var testObject = new Polls();
+	console.log("stuff" + img1 + img2 + description);
+	testObject.save({imageOne: img1, imageTwo: img2, description: description, imageOneVotes: 0, imageTwoVotes: 0});
 }
+)
